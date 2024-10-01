a1a98608fece42db24e608fb4484c7270a62f8a3
NguynBuu <quybuu713@gmail.com>
2024-10-01 22:11:45 +0700

    Buu sua bai6

--- bai6.java
+++ bai6.java
@@ -2,31 +2,30 @@ import java.util.Scanner;
 
 public class bai6 {
 	public static int n;
-	public static int chuso;
-	public static int daonguoc = 0;
+	public static int N;
 	public static void Nhap() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập vào số n: ");
         n=scanner.nextInt();
     }
     public static int DaoNguocSo() {
-        while (n != 0) {
-            chuso = n % 10;
+    	int daonguoc = 0;
+    	N=n;
+        while (N != 0) {
+            int chuso = N % 10;
             daonguoc = daonguoc * 10 + chuso;
-            n = n/ 10;
+            N = N/ 10;
         }
         return daonguoc;
     }
-    
     public static boolean KiemTraSoDoiXung() {
         return n == DaoNguocSo();
     }
-
     public static void Xuat() {
     	if (KiemTraSoDoiXung())
-            System.out.println("Là số đối xứng.");
+            System.out.println(n+" là số đối xứng.");
         else
-            System.out.println("Không phải là số đối xứng.");
+            System.out.println(n+" không phải là số đối xứng.");
     }
     public static void main(String[] args) {
         Nhap();
